2f089be9df36cfffdbbf7229f028b8d029be993f
G01\yamaoka-h <akoamay+schola@gmail.com>
2023-02-08 00:04:48 +0900

    目次挿入ブロックを実装

--- src/components/markdown/ScholaMarkdownViewer.tsx
+++ src/components/markdown/ScholaMarkdownViewer.tsx
@@ -9,8 +9,6 @@ import remarkMath from 'remark-math';
 
 import 'katex/dist/katex.min.css';
 import 'github-markdown-css/github-markdown.css';
-import remarkToc from 'remark-toc';
-import { visit } from 'unist-util-visit';
 
 import CodeBlock from 'components/markdown/CodeBlock';
 import { extractToc } from 'utils/extractToc';
@@ -19,25 +17,61 @@ import ImageBlock from './ImageBlock';
 
 import Link from 'next/link';
 
-const Toc = ({ props }) => {
-  const toc = extractToc();
-  return <span>Hello</span>;
-};
+import { parse } from 'path';
+
+const querystring = require('querystring');
 
 const ScholaMarkdownViewer = ({ children }) => {
   const toc = extractToc(children);
 
-  const TocBlock = ({ props }) => {
+  const TocBlock = ({ id, children }) => {
+    let depth = 0;
+
+    if (children?.length == 1) {
+      const parse = querystring.parse(children[0]);
+      if (!Number.isNaN(parseInt(parse.depth))) {
+        depth = parseInt(parse.depth);
+        if (depth < 1) depth = 1;
+        if (depth > 3) depth = 3;
+      }
+    }
+
+    const ListItem = ({ item }) => {
+      if (depth != 0 && item.depth > depth) return <></>;
+
+      const Lv1 = ({ item }) => (
+        <li>
+          <Link href={'#' + item.id}>
+            <a>{item.text}</a>
+          </Link>
+        </li>
+      );
+
+      const Lv2 = ({ item }) => (
+        <ul>
+          <Lv1 item={item} />
+        </ul>
+      );
+
+      const Lv3 = ({ item }) => (
+        <ul>
+          <Lv2 item={item} />
+        </ul>
+      );
+
+      if (item.depth == 1 || depth == 0) {
+        return <Lv1 item={item} />;
+      } else if (item.depth == 2) {
+        return <Lv2 item={item} />;
+      } else if (item.depth == 3) {
+        return <Lv3 item={item} />;
+      }
+    };
+
     return (
       <ul>
         {toc.map((item) => {
-          return (
-            <li>
-              <Link href={'#' + item.id}>
-                <a>{item.text}</a>
-              </Link>
-            </li>
-          );
+          return <ListItem item={item} />;
         })}
       </ul>
     );
